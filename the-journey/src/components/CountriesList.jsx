fa0c8e7ece64f6d686fc3d726dc0b01e6a1f47e5
jamesAkash <akash.james@outlook.com>
2023-09-15 17:40:53 +0530

    Countries component  added

--- the-journey/src/components/CountriesList.jsx
+++ the-journey/src/components/CountriesList.jsx
@@ -2,16 +2,30 @@ import React from "react";
 import styles from "./CountryList.module.css";
 import Spinner from "./Spinner";
 import Message from "./Message";
+import CountryItem from "./CountryItem";
 
 export default function CountriesList({ cities, isLoading }) {
-  const countries = cities?.map((city) => city.country);
+  //   const countries = cities?.map((city) => city.country);
+  //   const countries = cities.reducer((acc,cur)=>
+  const countries = cities.reduce((arr, city) => {
+    if (!arr.map((el) => el.country).includes(city.country))
+      return [
+        ...arr,
+        {
+          country: city.country,
+          emoji: city.emoji,
+          id: arr.length + 1,
+        },
+      ];
+    else return arr;
+  }, []);
   if (isLoading) return <Spinner />;
   if (!cities.length)
     return <Message message="Add your first city by clicking on the map" />;
   return (
-    <ul className={styles.cityList}>
-      {countries.map((country, i) => (
-        <p key={i}>{country}</p>
+    <ul className={styles.countryList}>
+      {countries.map((country) => (
+        <CountryItem country={country} key={country.id} />
       ))}
     </ul>
   );
