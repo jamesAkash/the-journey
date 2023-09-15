6a39ca36de01112711a1a55aa40ae641432d1c4f
jamesAkash <akash.james@outlook.com>
2023-09-15 18:45:04 +0530

    searchParams to store global states

--- the-journey/src/components/CityItem.jsx
+++ the-journey/src/components/CityItem.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from "./CityItem.module.css";
+import { Link } from "react-router-dom";
 
 const formatDate = (date) =>
   new Intl.DateTimeFormat("en", {
@@ -9,13 +10,19 @@ const formatDate = (date) =>
   }).format(new Date(date));
 
 export default function CityItem({ city }) {
-  const { cityName, emoji, date } = city;
+  const { cityName, emoji, date, id, position } = city;
+
   return (
-    <li className={styles.cityItem}>
-      <span className={styles.emoji}>{emoji}</span>
-      <h3 className={styles.name}>{cityName}</h3>
-      <time className={styles.date}>({formatDate(date)})</time>
-      <button className={styles.deleteBtn}>&times;</button>
+    <li>
+      <Link
+        to={`${id}?lat=${position.lat}&lng=${position.lng}`}
+        className={styles.cityItem}
+      >
+        <span className={styles.emoji}>{emoji}</span>
+        <h3 className={styles.name}>{cityName}</h3>
+        <time className={styles.date}>({formatDate(date)})</time>
+        <button className={styles.deleteBtn}>&times;</button>
+      </Link>
     </li>
   );
 }
--- the-journey/src/components/Map.jsx
+++ the-journey/src/components/Map.jsx
@@ -1,6 +1,28 @@
 import React from "react";
 import styles from "./Map.module.css";
+import { useSearchParams } from "react-router-dom";
 
 export default function Map() {
-  return <div className={styles.mapContainer}>Map</div>;
+  const [searchParams, setSearchParams] = useSearchParams();
+  const lat = searchParams.get("lat");
+  const lng = searchParams.get("lng");
+
+  return (
+    <div className={styles.mapContainer}>
+      <h1>Map</h1>
+      <h3>
+        Position: {lat} {lng}
+      </h3>
+      <button
+        onClick={() =>
+          setSearchParams({
+            lat: 23,
+            lng: 50,
+          })
+        }
+      >
+        chnage pos
+      </button>
+    </div>
+  );
 }
